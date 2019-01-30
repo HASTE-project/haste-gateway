17405f1521844686329e965121a6a60c25f1ec6d
Ben Blamey <blamey.ben@gmail.com>
2019-01-30 01:48:47 +0100

    namespace packages

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup
 
 setup(name='haste_cloud_gateway',
       version='0.10',
-      packages=[
-                'haste.cloud_gateway'],
+      packages=['haste.cloud_gateway'],
+      namespace_packages=['haste'],
       description='Cloud Gateway for the HASTE platform: http://http://haste.research.it.uu.se',
       author='Ben Blamey',
       author_email='ben.blamey@it.uu.se',
